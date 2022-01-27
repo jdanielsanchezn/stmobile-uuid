ab540268e8ac562b057651fcce60faa8f0d56f62
Jose Daniel Sanchez <joses@medallia.com>
2022-01-27 15:41:22 -0300

    implement code generator and copy features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
+import "./utilities.css";
 
 function App() {
+  const [id, setId] = useState("-");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">ST Mobile uuidv4 code generator</header>
+      <div className="flex justify-center">
+        <h2 className="margin-xlarge white">{id}</h2>
+      </div>
+      <button className="button margin-small" onClick={() => setId(uuidv4())}>
+        generate a new uuidv4 code
+      </button>
+      <button
+        className="button margin-small"
+        onClick={() => {
+          navigator.clipboard.writeText(id);
+        }}
+      >
+        copy to clipboard
+      </button>
     </div>
   );
 }
